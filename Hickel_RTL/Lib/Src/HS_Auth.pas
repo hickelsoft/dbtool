5a2553974028dc259204b5e40f10866c0dc672b5
Daniel Marschall <28412477+danielmarschall@users.noreply.github.com>
2026-06-07 21:22:10 +0200

    Release new version

--- Hickel_RTL/Lib/Src/HS_Auth.pas
+++ Hickel_RTL/Lib/Src/HS_Auth.pas
@@ -11,6 +11,7 @@ function HS_SA_DB_PASSWORD: WideString;
 function HS_SA_DB_USER: WideString;
 function HS_ALUV4_HMAC_SECRET: WideString;
 function make_code(KundenNr: integer; SerienNr: integer; Dateianfrage: integer): WideString;
+function GetHskStatic(Number: Cardinal): WideString;
 
 implementation
 
@@ -30,45 +31,24 @@ begin
 end;
 
 function HS_SA_DB_PASSWORD: WideString;
-var
-  Len: DWORD;
 begin
-  if not Trusted_HSK then exit('');
-  Len := HSK_0003(HS_SA_DB_OLD, nil);
-  SetLength(Result, Len);
-  if Len > 0 then
-  begin
-    Assert(Len = HSK_0003(HS_SA_DB_OLD, PWideChar(Result)));
-    SetLength(Result, Len-1); // Null Terminator entfernen
-  end;
+  if HS_SA_DB_OLD then
+    result := GetHskStatic(HSK_0003_Compat)
+  else
+    result := GetHskStatic(HSK_0003_Compat+1);
 end;
 
 function HS_SA_DB_USER: WideString;
-var
-  Len: DWORD;
 begin
-  if not Trusted_HSK then exit('');
-  Len := HSK_0004(HS_SA_DB_OLD, nil);
-  SetLength(Result, Len);
-  if Len > 0 then
-  begin
-    Assert(Len = HSK_0004(HS_SA_DB_OLD, PWideChar(Result)));
-    SetLength(Result, Len-1); // Null Terminator entfernen
-  end;
+  if HS_SA_DB_OLD then
+    result := GetHskStatic(HSK_0004_Compat)
+  else
+    result := GetHskStatic(HSK_0004_Compat+1);
 end;
 
 function HS_ALUV4_HMAC_SECRET: WideString;
-var
-  Len: DWORD;
 begin
-  if not Trusted_HSK then exit('');
-  Len := HSK_0005(nil);
-  SetLength(Result, Len);
-  if Len > 0 then
-  begin
-    Assert(Len = HSK_0005(PWideChar(Result)));
-    SetLength(Result, Len-1); // Null Terminator entfernen
-  end;
+  result := GetHskStatic(HSK_0005_Compat);
 end;
 
 function make_code(KundenNr: integer; SerienNr: integer; Dateianfrage: integer): WideString;
@@ -81,6 +61,20 @@ begin
   if Len > 0 then
   begin
     Assert(Len = HSK_0006(KundenNr, SerienNr, Dateianfrage, PWideChar(Result)));
+    SetLength(Result, Len-1); // Null Terminator entfernen
+  end;
+end;
+
+function GetHskStatic(Number: Cardinal): WideString;
+var
+  Len: DWORD;
+begin
+  if not Trusted_HSK then exit('');
+  Len := HSK_0014(Number, nil);
+  SetLength(Result, Len);
+  if Len > 0 then
+  begin
+    Assert(Len = HSK_0014(Number, PWideChar(Result)));
     SetLength(Result, Len-1); // Null Terminator entfernen
   end;
 end;
--- Hickel_RTL/Lib/Src/hl_HSK.pas
+++ Hickel_RTL/Lib/Src/hl_HSK.pas
@@ -12,17 +12,19 @@ function Trusted_HSK: boolean;
 function HSK_0000(A: UInt64): UInt64;
 function HSK_0001: BOOL;
 function HSK_0002(A: PWideChar): BOOL;
-function HSK_0003(A: BOOL; B: PWideChar): DWORD;
-function HSK_0004(A: BOOL; B: PWideChar): DWORD;
-function HSK_0005(A: PWideChar): DWORD;
+const HSK_0003_Compat = 0;
+const HSK_0004_Compat = 0;
+const HSK_0005_Compat = 0;
 function HSK_0006(A: integer; B: integer; C: integer; D: PWideChar): DWORD;
 function HSK_0007(A: PWideChar; B: BOOL; C: PWideChar): DWORD;
 function HSK_0008: DWORD;
 procedure HSK_0009(A: PWideChar; B: integer; C: Int64);
 procedure HSK_0010;
 procedure HSK_0011(A: PWideChar; B: PByte);
-function HSK_0012(A: PWideChar): DWORD;
+const HSK_0012_Compat = 0;
 function HSK_0013: Double;
+function HSK_0014(A: DWORD; B: PWideChar): DWORD;
+const HSK_0015_Compat = 0;
 
 implementation
 
@@ -46,21 +48,6 @@ begin
   exit(false);
 end;
 
-function HSK_0003(A: BOOL; B: PWideChar): DWORD;
-begin
-  exit(0);
-end;
-
-function HSK_0004(A: BOOL; B: PWideChar): DWORD;
-begin
-  exit(0);
-end;
-
-function HSK_0005(A: PWideChar): DWORD;
-begin
-  exit(0);
-end;
-
 function HSK_0006(A: integer; B: integer; C: integer; D: PWideChar): DWORD;
 begin
   exit(0);
@@ -91,12 +78,12 @@ begin
   exit;
 end;
 
-function HSK_0012(A: PWideChar): DWORD;
+function HSK_0013: Double;
 begin
   exit(0);
 end;
 
-function HSK_0013: Double;
+function HSK_0014(A: DWORD; B: PWideChar): DWORD;
 begin
   exit(0);
 end;
